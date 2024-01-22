ce421a7c635103b2e1406debfc3dd4046ab1d88e
Lukasz <lukaszbialko@o2.pl>
2024-01-22 20:33:44 +0100

    code corrections and use prettier

--- js/script.js
+++ js/script.js
@@ -4,29 +4,27 @@ const square = document.querySelector('.color')
 const restoreColor = document.querySelector('.remove-color')
 
 const redColor = () => {
-    square.style.backgroundColor = 'red'
-
-    if(square.style.contains = 'red') {
-        redBtn.style.color = 'tomato'
-        blueBtn.style.color = '#fff'
-    }
+	square.style.backgroundColor = 'red'
+	if ((square.style.contains = 'red')) {
+		redBtn.style.color = 'tomato'
+		blueBtn.style.color = '#fff'
+	}
 }
 
 const blueColor = () => {
-    square.style.backgroundColor = 'blue'
-
-    if(square.style.contains = 'blue') {
-        blueBtn.style.color = 'royalblue'
-        redBtn.style.color = '#fff'
-    }
+	square.style.backgroundColor = 'blue'
+	if ((square.style.contains = 'blue')) {
+		blueBtn.style.color = 'royalblue'
+		redBtn.style.color = '#fff'
+	}
 }
 
 const deleteColor = () => {
-    square.style.backgroundColor = 'orange'
-    if(square.style.contains = 'blue' || 'red') {
-        blueBtn.style.color = '#fff'
-        redBtn.style.color = '#fff'
-    }
+	square.style.backgroundColor = 'orange'
+	if ((square.style.contains = 'blue' || 'red')) {
+		blueBtn.style.color = '#fff'
+		redBtn.style.color = '#fff'
+	}
 }
 
 redBtn.addEventListener('click', redColor)
